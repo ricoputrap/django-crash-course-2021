5620bd9f8474e975f0e1ad86f119822b605a3bac
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-05-31 06:04:32 +0700

    Implement a model and pass the data to the template view

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=155)
+  content = models.TextField()
+  slug = models.SlugField(max_length=255)
+
+  def __str__(self):
+    return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home(request):
-  return render(request, "index.html", {})
+  # get all posts
+  data = Post.objects.all()
+
+  # render the template by passing those "posts"
+  return render(request, "index.html", {"posts":data})
